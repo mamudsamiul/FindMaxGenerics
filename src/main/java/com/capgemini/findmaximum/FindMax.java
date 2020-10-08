cde4043c041dceeeb86a828b3b4c7605c3fad4f3
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-09 01:08:42 +0530

    (Samiul Mamud) Refactored:One generic method created

--- src/main/java/com/capgemini/findmaximum/FindMax.java
+++ src/main/java/com/capgemini/findmaximum/FindMax.java
@@ -1,8 +1,8 @@
 package com.capgemini.findmaximum;
 
 public class FindMax {
-	public static String find(String numberOne, String numberTwo, String numberThree) {
-		String max = numberOne;
+	public static <E extends Comparable<E>> E find(E numberOne, E numberTwo, E numberThree) {
+		E max = numberOne;
 		if (numberTwo.compareTo(max) > 0)
 			max = numberTwo;
 		if (numberThree.compareTo(max) > 0)
