1bb224340fb7639c103b47af6c669a037cf7a67d
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-09 01:53:08 +0530

    (Samiul Mamud) Refactored: Refactor to create generic class to take in 3 variables of generic type

--- src/main/java/com/capgemini/findmaximum/FindMax.java
+++ src/main/java/com/capgemini/findmaximum/FindMax.java
@@ -1,6 +1,20 @@
 package com.capgemini.findmaximum;
 
-public class FindMax {
+public class FindMax<E extends Comparable<E>> {
+	E numberOne;
+	E numberTwo;
+	E numberThree;
+
+	public FindMax(E numberOne, E numberTwo, E numberThree) {
+		this.numberOne = numberOne;
+		this.numberTwo = numberTwo;
+		this.numberThree = numberThree;
+	}
+
+	public E maximum() {
+		return find(numberOne, numberTwo, numberThree);
+	}
+
 	public static <E extends Comparable<E>> E find(E numberOne, E numberTwo, E numberThree) {
 		E max = numberOne;
 		if (numberTwo.compareTo(max) > 0)
